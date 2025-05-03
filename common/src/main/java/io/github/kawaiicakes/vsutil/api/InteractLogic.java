cefde64f9fff140ed88f26236905773b7078800a
kawaiicakes <vomitmopper@gmail.com>
2025-05-03 11:45:07 -0600

    New hacky fix that hopefully actually works this time

--- common/src/main/java/io/github/kawaiicakes/vsutil/api/InteractLogic.java
+++ common/src/main/java/io/github/kawaiicakes/vsutil/api/InteractLogic.java
@@ -6,30 +6,32 @@ import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.Pose;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
+import org.slf4j.Logger;
 
 public class InteractLogic {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
     public static void interactWith(ServerPlayer player, ServerLevel level, BlockPos blockPos) {
         BlockHitResult hitResult = new BlockHitResult(
                 new Vec3(blockPos.getX(), blockPos.getY(), blockPos.getZ()), Direction.UP, blockPos, true
         );
 
-        final Pose playerPose = player.getPose();
+        final boolean shiftKeyDown = player.getSharedFlag(1);
 
         // this seems dangerous LOL
         try {
-            player.setPose(Pose.STANDING);
+            player.setSharedFlag(1, false);
             player.gameMode.useItemOn(
                     player, level, ItemStack.EMPTY, InteractionHand.MAIN_HAND, hitResult
             );
         } catch (Exception e) {
-            LogUtils.getLogger().error("Error interacting!", e);
-            player.setPose(playerPose);
+            LOGGER.error("Error interacting!", e);
+            player.setSharedFlag(1, shiftKeyDown);
         }
 
-        player.setPose(playerPose);
+        player.setSharedFlag(1, shiftKeyDown);
     }
 }
